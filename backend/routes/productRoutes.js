386b16ccd8149af0700624fa40c6d1654d80d3fd
Kisan-jena <kisan.k.k.jena.01@gmail.com>
2025-04-01 18:56:15 +0530

    + admin auth and get start with admin dashboard UI

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express, { Router } from 'express'
 import {addProduct,listProduct,removeProduct,singleProduct} from '../controllers/productController.js'
 import upload from '../middlewares/uploadMiddleware.js'
+import adminAuth from '../middlewares/adminAuth.js'
 
 const router=express.Router
 const productRouter = router()
@@ -13,8 +14,8 @@ const uploadMultipleFields = upload.fields([
     { name: 'image4', maxCount: 1 }
 ]);
 
-productRouter.post('/add',uploadMultipleFields,addProduct)
-productRouter.post('/remove',removeProduct)
+productRouter.post('/add',uploadMultipleFields,adminAuth,addProduct)
+productRouter.post('/remove',adminAuth,removeProduct)
 productRouter.post('/singleProduct',singleProduct) //^ OR using get ,productRouter.get('/singleProduct/:id',singleProduct)
 productRouter.get('/listProduct',listProduct)
 
