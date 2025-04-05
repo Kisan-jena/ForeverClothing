4b0baadff6f05927ef5f6c487eb0833c4b08aba3
Kisan-jena <kisan.k.k.jena.01@gmail.com>
2025-04-05 20:25:33 +0530

    added the logout features and also stored token in localstorage to prevet relogin on reload

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {Routes,Route} from "react-router-dom" 
 
 import {ToastContainer} from 'react-toastify'
@@ -14,7 +13,11 @@ import Login from './components/Login';
 
 const App = () => {
 
-  const [token,setToken]=useState("")
+  const [token,setToken]=useState(localStorage.getItem('token') ? localStorage.getItem('token'):"")
+
+  useEffect(()=>{
+    localStorage.setItem('token',token)
+  },[token])
    
 
   return (
@@ -23,15 +26,15 @@ const App = () => {
       {
         token === '' ?<Login setToken={setToken} /> :         
         <>
-          <Navbar/>
+          <Navbar setToken={setToken} />
           <hr />
           <div className='flex w-full' >
             <Sidebar/>
             <div className='w-[70%] mx-auto ml-[max(5vw,25px) my-8 text-gray-600 text-base]' >
               <Routes>
-                <Route path='/add' element={<Add/>} />
-                <Route path='/list'element={<List/>} />
-                <Route path='/orders' element={<Orders/>} />
+                <Route path='/add' element={<Add/>} token={token} />
+                <Route path='/list'element={<List/>} token={token} />
+                <Route path='/orders' element={<Orders/>} token={token} />
               </Routes>
             </div>
           </div>
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import assets from '../assets/assets'
+import { toast } from 'react-toastify';
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setToken('');
+    toast.success("Logged out successfully!");
+  }
+
   return (
     <div className='flex items-center py-2 px-[4%] justify-between b-amber-100 ' >
       <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-      <button className='bg-gray-600 text-white px-5 py-3 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+      <button onClick={handleLogout} className='bg-gray-600 text-white px-5 py-3 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
